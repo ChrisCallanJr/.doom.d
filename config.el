097511d9966b1fc48d9394b2cbb897862bb962da
Chris Callan <chriscallanjr@me.com>
2019-09-05 13:21:16 -0400

    changes splash image

--- config.el
+++ config.el
@@ -1,7 +1,11 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-(load-theme 'doom-opera t)
+(load-theme 'doom-opera-light t)
+
+(setq
+ fancy-splash-image "~/Documents/Shahada.svg"
+ )
 
 (setq
  mac-command-modifier 'meta
