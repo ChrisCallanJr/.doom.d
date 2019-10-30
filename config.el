c5dad6806f47451f582feb6649c4c67b407d76e0
Chris Callan <chriscallanjr@me.com>
2019-10-30 10:16:43 -0400

    switches to Input Serif

--- config.el
+++ config.el
@@ -1,14 +1,14 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-(load-theme 'doom-oceanic-next t)
+(load-theme 'doom-nord-light t)
 
 (setq
  display-line-numbers-type 'relative
  )
 
 (setq
- doom-font (font-spec :family "Input Mono" :size 20)
+ doom-font (font-spec :family "Input Serif" :size 20)
  )
 
 (setq
